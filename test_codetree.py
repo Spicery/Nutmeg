4f56614729f55f16e575a3b3cedafa08ec2477e4
Stephen Leach <sfkleach@gmail.com>
2020-08-14 10:08:57 +0100

    Amending if-codelet to support test/then/else fields

---
+++ test_codetree.py
@@ -0,0 +1,14 @@
+import codetree
+import io
+
+def test_deserialise():
+    # Arrange
+    jdata = '{ "kind":"if", "test": {"kind":"bool", "value":"true"}, "then": {"kind":"string", "value":"yes"}, "else": {"kind":"string", "value":"yes"} }'
+    # Act
+    data = codetree.deserialise( io.StringIO( jdata ) )
+    # Assert
+    assert isinstance( data, codetree.IfCodelet )
+    assert isinstance( data._test, codetree.BoolCodelet )
+    assert isinstance( data._then, codetree.StringCodelet )
+    assert isinstance( data._else, codetree.StringCodelet )
+
