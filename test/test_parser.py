19add58788c33898cbcd9f918d87ea13a001577d
Zack Adlington <zadlington@gmail.com>
2020-09-03 19:30:36 +0100

    Test token generator for identifiers

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,15 +1,34 @@
 import parser
 import re
+import json
 from collections import namedtuple
 
 Token = namedtuple("Token", ["type", "value"])
+
+
+def help_test_token_generator(regex, input, expected_output):
+    generator = parser.generate_tokens(re.compile(regex), input)
+    range_index = 0
+
+    for actual in generator:
+        assert range_index + 1 <= len(
+            expected_output
+        ), "Too many values returned from range"
+        assert expected_output[range_index] == actual
+        range_index += 1
+
+    assert range_index == len(expected_output), "Too few values returned from range"
 
 
 def test_compiles_regex():
     assert type(parser.compile_regex({"BIND": "(?P<BIND>:=)",})) == re.Pattern
 
 
-def test_generates_tokens():
+def test_generates_number_tokens():
+    help_test_token_generator(
+        r"(?P<NUM>\d+)", input="42", expected_output=[Token(type="NUM", value="42")],
+    )
+
     generator = parser.generate_tokens(re.compile(r"(?P<NUM>\d+)"), "42")
     expected_values = [Token(type="NUM", value="42")]
 
@@ -24,17 +43,10 @@ def test_generates_tokens():
     assert range_index == len(expected_values), "Too few values returned from range"
 
 
-def test_int_literals():
-    assert parser.tokenize("42") == [Token(type="NUM", value="42")]
+def test_generates_identifier_tokens():
+    help_test_token_generator(
+        r"(?i)(?P<ID>^([a-z])\w*)",
+        input="x",
+        expected_output=[Token(type="ID", value="x")],
+    )
 
-
-def test_string_literals():
-    assert parser.tokenize("'string with single quotes'") == [
-        Token(type="STRING", value="'string with single quotes'")
-    ]
-    assert parser.tokenize('"string with double quotes"') == [
-        Token(type="STRING", value='"string with double quotes"')
-    ]
-    assert parser.tokenize("\"string within a 'string'\"") == [
-        Token(type="STRING", value="\"string within a 'string'\"")
-    ]
