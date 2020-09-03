2ea5fa4d7e3125498f76d3a2c6d490402d121bb1
Zack Adlington <zadlington@gmail.com>
2020-09-03 15:50:32 +0100

    Create ExpressionEvaluator class

--- parser.py
+++ parser.py
@@ -13,18 +13,6 @@ import token_specification
 Token = namedtuple("Token", ["type", "value"])
 
 
-def tokenize(text):
-    token_spec = flatten(token_specification.tokens)
-    regex_pattern = compile_regex(token_spec)
-
-    tokens = []
-
-    for tok in generate_tokens(regex_pattern, text):
-        tokens.append(tok)
-
-    return tokens
-
-
 def flatten(token_dictionary):
     """
     Take a two-dimensional dictionary in form "<category>": {"<token name>" : "<regex pattern>"}.
@@ -54,4 +42,40 @@ def compile_regex(flat_token_specification):
 def generate_tokens(regex_pattern, text):
     scanner = regex_pattern.scanner(text)
     for m in iter(scanner.match, None):
-        yield Token(m.lastgroup, m.group())
+        tok = Token(m.lastgroup, m.group())
+        if tok.type != "WS":
+            yield tok
+
+
+class ExpressionEvaluator:
+    """
+    docstring
+    """
+
+    def __init__(self):
+        self.regex = compile_regex(flatten(token_specification.tokens))
+
+    def parse(self, text):
+        self.tokens = generate_tokens(self.regex, text)
+        self.tok = None  # Last symbol consumed
+        self.nexttok = None  # Next symbol tokenized
+        self._advance()  # Load first lookahead token
+
+        return self.expr()
+
+    def _advance(self):
+        "Advange one token ahead"
+        self.tok, self.nexttok = self.nexttok, next(self.tokens, None)
+
+    def _accept(self, toktype):
+        "Test and consume the next token if it matches toktype"
+        if self.nexttok and self.nexttok.type == toktype:
+            self._advance()
+            return True
+        else:
+            return False
+
+    def _expect(self, toktype):
+        "Consume next token if it matches toktype or raise SyntaxError"
+        if not self._accept(toktype):
+            raise SyntaxError("Expected " + toktype)
