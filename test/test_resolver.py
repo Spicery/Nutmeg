dbc1b10ba50fd0c5f983e2dbfd349a1bd9cbbbc0
Stephen Leach <sfkleach@gmail.com>
2020-10-01 00:21:34 +0100

    Passing unit tests for the resolver

--- test/test_resolver.py
+++ test/test_resolver.py
@@ -5,35 +5,37 @@ import codetree
 import resolver
 import sys
 
+def resolveCodeTree( tree ):
+    resolver.Resolver().resolveCodeTree( tree )
+
 def test_resolveID():
     # Arrange
-    codelet = {
+    jcodelet = {
         "kind": "id",
         "name": "x",
         "reftype": "get"
     }
-    codelet = codetree.deserialise( io.StringIO( json.dumps( codelet ) ) )
+    codelet = codetree.CodeTreeFromJSONObject( jcodelet )
     # Act
-    resolver.Resolver().resolveCodeTree( codelet )
+    resolveCodeTree( codelet )
     # Assert
     assert 'global' == codelet.scope()
-    codelet.serialise(sys.stderr)
 
 def test_resolveLiteral():
     # Arrange
-    codelet = {
+    jcodelet = {
         "kind": "string",
         "value": "hello world"
     }
-    codelet = codetree.deserialise( io.StringIO( json.dumps( codelet ) ) )
+    codelet = codetree.CodeTreeFromJSONObject( jcodelet )
     # Act
-    resolver.Resolver().resolveCodeTree( codelet )
+    resolveCodeTree( codelet )
     # Assert
-    # TODO: Got here - to be completed.
+    assert jcodelet == codelet.toJSON()
 
 def test_resolveBinding():
     # Arrange
-    codelet = {
+    jcodelet = {
         "kind": "binding",
         "lhs": {
             "kind": "id",
@@ -45,9 +47,102 @@ def test_resolveBinding():
             "value": "hello world"
         }
     }
-    codelet = codetree.deserialise( io.StringIO( json.dumps( codelet ) ) )
+    codelet = codetree.CodeTreeFromJSONObject( jcodelet )
     # Act
-    resolver.Resolver().resolveCodeTree( codelet )
+    resolveCodeTree( codelet )
     # Assert
-    codelet.serialise(sys.stderr)
+    assert "global" == codelet.lhs().scope()
 
+def test_resolveFunction():
+    # Arrange
+    jcodelet = {
+        "kind": "function",
+        "parameters": {
+            "kind": "id",
+            "name": "x",
+            "reftype": "val"
+        },
+        "body": {
+            "kind": "id",
+            "name": "x",
+            "reftype": "get"
+        }
+    }
+    codelet = codetree.CodeTreeFromJSONObject( jcodelet )
+    # Act
+    resolveCodeTree( codelet )
+    # Assert
+    assert "local" == codelet.body().scope()
+    assert codelet.parameters().label() == codelet.body().label()
+    assert codelet.parameters().nonassignable() and codelet.body().nonassignable()
+    assert not codelet.parameters().immutable() and not codelet.body().immutable()
+
+def test_resolveFunction2Variables():
+    # Arrange
+    jcodelet = {
+        "kind": "function",
+        "parameters": {
+            "kind":"seq",
+            "body": [
+                {
+                    "kind": "id",
+                    "name": "x",
+                    "reftype": "val"
+                },
+                {
+                    "kind": "id",
+                    "name": "y",
+                    "reftype": "var"
+                }
+            ]
+        },
+        "body": {
+            "kind": "id",
+            "name": "x",
+            "reftype": "get"
+        }
+    }
+    codelet = codetree.CodeTreeFromJSONObject( jcodelet )
+    # Act
+    resolveCodeTree( codelet )
+    # Assert
+    assert "local" == codelet.body().scope()
+    params = [*codelet.parameters().members()]
+    assert params[0].label() == codelet.body().label()
+    assert params[1].label() != codelet.body().label()
+    assert params[0].nonassignable() and codelet.body().nonassignable()
+    assert not params[0].immutable() and not codelet.body().immutable()
+    assert not params[1].nonassignable()
+    assert not params[1].immutable()
+    assert params[0].scope() == "local"
+    assert params[1].scope() == "local"
+    assert params[0].reftype() == "new"
+    assert params[1].reftype() == "new"
+
+def test_resolveIf():
+    # Arrange
+    jcodelet = {
+        "kind": "if",
+        "test": {
+            "kind": "id",
+            "name": "x",
+            "reftype": "get"
+        },
+        "then":  {
+            "kind": "id",
+            "name": "y",
+            "reftype": "get"
+        },
+        "else": {
+            "kind": "id",
+            "name": "z",
+            "reftype": "get"
+        }
+    }
+    codelet = codetree.CodeTreeFromJSONObject( jcodelet )
+    # Act
+    resolveCodeTree( codelet )
+    # Assert
+    assert "global" == codelet.testPart().scope()
+    assert "global" == codelet.thenPart().scope()
+    assert "global" == codelet.elsePart().scope()
