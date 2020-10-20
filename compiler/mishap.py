22eccc898ad89eaefec41a460f30fc3672548445
Stephen Leach <sfkleach@gmail.com>
2020-10-20 19:31:57 +0100

    Slight tweak to the Mishap class.

--- compiler/mishap.py
+++ compiler/mishap.py
@@ -1,12 +1,18 @@
 
 class Mishap( Exception ):
+    """
+    Use this class for raising errors inside the compiler. The top-level
+    of the compiler traps mishap exceptions and reports the error message
+    and all the keyword arguments that have been passed in.
+    """
 
     def __init__( self, *args, **kwargs ):
         super().__init__( *args )
         self._kwargs_list = [ kwargs ]
 
-    def culprit( self, **kwargs ):
+    def addDetails( self, **kwargs ):
         self._kwargs_list.append( kwargs )
+        return self
 
     def items( self ):
         for kwargs in self._kwargs_list:
