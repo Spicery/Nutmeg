a1caa724d78bf8681b8059f32655f4a726dc262d
Stephen Leach <sfkleach@gmail.com>
2020-12-27 10:01:57 +0000

    Add the `nutmeg script` command

--- scripts/mkbinnutmeg.py
+++ scripts/mkbinnutmeg.py
@@ -6,7 +6,13 @@ import argparse
 TEMPLATE = """#!/bin/bash
 case $1 in
     compile|parse|resolve|optimize|codegen|bundle|trace)
-        exec /opt/nutmeg/libexec/nutmeg/compiler/nutmeg $*
+        exec $(INSTALL_DIR)/compiler/nutmeg $*
+        ;;
+    script)
+        shift
+        tfile=$(mktemp -u)
+        $(INSTALL_DIR)/compiler/nutmeg compile --bundle="$tfile" $* && $(INSTALL_DIR)/runner/NutmegRunner "$tfile"
+        /bin/rm -f "$tfile"
         ;;
     unittest)
         shift
