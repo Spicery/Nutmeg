69dc9b73053334d783c0253976fed7bf757f5473
Stephen Leach <sfkleach@gmail.com>
2021-01-06 17:40:01 +0000

    Add the range system function

--- runner/NutmegRunner/RangesModule.cs
+++ runner/NutmegRunner/RangesModule.cs
@@ -1,5 +1,30 @@
 ﻿namespace NutmegRunner.Modules.Ranges {
 
+    public class RangeSystemFunction : VariadicSystemFunction {
+
+        public RangeSystemFunction( Runlet next ) : base( next ) { }
+
+        public override Runlet ExecuteRunlet( RuntimeEngine runtimeEngine ) {
+            int n = runtimeEngine.ValueStackLength();
+            long step = 1;
+            switch (n) {
+                case 2:
+                    break;
+                case 3:
+                    step = (long)runtimeEngine.PopValue();
+                    break;
+                default:
+                    throw new NutmegException( "Wrong number of arguments for range" ).Culprit( "Expecting", "2 or 3 arguments" ).Culprit( "Received", $"{n}" );
+            }
+            long y = (long)runtimeEngine.PopValue();
+            long x = (long)runtimeEngine.PopValue();
+            var list = HalfOpenRangeList.New( x, y, step );
+            runtimeEngine.PushValue( list );
+            return this.Next;
+        }
+
+    }
+
     public class HalfOpenRangeListSystemFunction : FixedAritySystemFunction {
 
         public HalfOpenRangeListSystemFunction( Runlet next ) : base( next ) { }
@@ -9,7 +34,7 @@
         public override Runlet ExecuteRunlet( RuntimeEngine runtimeEngine ) {
             long y = (long)runtimeEngine.PopValue();
             long x = (long)runtimeEngine.PopValue();
-            var list = new HalfOpenRangeList( x, y );
+            var list = HalfOpenRangeList.New( x, y );
             runtimeEngine.PushValue( list );
             return this.Next;
         }
@@ -25,7 +50,7 @@
         public override Runlet ExecuteRunlet( RuntimeEngine runtimeEngine ) {
             long y = (long)runtimeEngine.PopValue();
             long x = (long)runtimeEngine.PopValue();
-            var list = new HalfOpenRangeList( x, y + 1 );
+            var list = HalfOpenRangeList.New( x, y + 1 );
             runtimeEngine.PushValue( list );
             return this.Next;
         }
@@ -67,8 +92,11 @@
     }
 
 
+
+
     public class RangesModule : SystemFunctionsModule {
         public override void AddAll() {
+            Add( "range", r => new RangeSystemFunction( r ) );
             Add( "..<", r => new HalfOpenRangeSystemFunction( r ), "halfOpenRange" );
             Add( "...", r => new ClosedRangeSystemFunction( r ), "closedRange" );
             Add( "[x..<y]", r => new HalfOpenRangeListSystemFunction( r ), "halfOpenRangeList" );
